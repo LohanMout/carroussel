514f8216af66e5003d56d597539c51292bc816e3
LohanMout <e2155871@cmaisonneuve.qc.ca>
2024-04-18 12:02:38 -0400

    ajout d'image dans le carroussel

--- js/carroussel.js
+++ js/carroussel.js
@@ -1,3 +1,30 @@
 (function(){
-    console.log("javascript");
+    let carroussel = document.querySelector('.carroussel');
+    let btn = document.querySelector('.btn__ouvrir');
+    let carroussel__x = document.querySelector('.carroussel__x');
+    let galerie = document.querySelector('.galerie');
+    let carrousselFigure = document.querySelector('.carroussel__figure')
+    let carrousselImg = document.createElement('img');
+    let galerieImg = galerie.querySelectorAll('img')
+    carrousselImg.classList.add('carroussel__img');
+    //carrousselImg.src = galerieImg.src;
+    //carrousselFigure.appendChild(carrousselImg)
+
+    for(const elm of galerieImg){
+        console.log(elm.src);
+    }
+
+    btn.addEventListener('mousedown', ouvrirGallerie);
+    carroussel__x.addEventListener('mousedown', fermerGalerie);
+
+
+    function ouvrirGallerie(){
+        carroussel.classList.add("carroussel--ouvrir");
+        console.log("ouvrir");
+    }
+
+    function fermerGalerie(){
+        carroussel.classList.remove("carroussel--ouvrir");
+        console.log("fermer");
+    }
 })()

30a9518a4f3d3ed83feb1ee5944fc3c871bf58a7
LohanMout <e2155871@cmaisonneuve.qc.ca>
2024-04-24 09:48:34 -0400

    debut labo5

--- js/carroussel.js
+++ js/carroussel.js
@@ -4,18 +4,43 @@
     let carroussel__x = document.querySelector('.carroussel__x');
     let galerie = document.querySelector('.galerie');
     let carrousselFigure = document.querySelector('.carroussel__figure')
-    let carrousselImg = document.createElement('img');
     let galerieImg = galerie.querySelectorAll('img')
-    carrousselImg.classList.add('carroussel__img');
+    let index = 0;
+
     //carrousselImg.src = galerieImg.src;
     //carrousselFigure.appendChild(carrousselImg)
 
     for(const elm of galerieImg){
         console.log(elm.src);
+        creerCarrousselImg(index,elm);
+        index = index + 1;
+    }
+
+    /**
+     * @param {*} index
+     * @param elm
+     */
+
+    function creerCarrousselImg(index,elm){
+        let carrousselImg = document.createElement('img');
+        carrousselImg.classList.add('carroussel__img');
+        carrousselImg.src = elm.src;
+        carrousselImg.dataset.index = index;
+        carrousselFigure.appendChild(carrousselImg);
+    }
+
+    /** 
+     * @param {*} index 
+     */
+
+    function creerRadioImg(index){
+        let carrousselRadio = document.createElement('input');
+        carrousselRadio.type = 'radio';
     }
 
     btn.addEventListener('mousedown', ouvrirGallerie);
     carroussel__x.addEventListener('mousedown', fermerGalerie);
+
 
 
     function ouvrirGallerie(){
